606cf94091b3475bd0b488f8f8a5b4d52e626e53
Matt Osborne <MattJOzzy@gmail.com>
2023-02-02 22:57:51 -0600

    finished javascript functions, completed readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Work Day Scheduler
 
-In this project I created an app that helps schedule the user's work day.
+In this project I created an app that helps schedule a user's work day. Users can enter notes into each hour of their work day and save them to their browser's local storage. I used jQuery in this project to select and modify certain elements of the page. I also used the API dayJs to determine the users time and date. 
+
+![Screenshot](./images/Screenshot.jpg)
--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-
-  });
-
 // Displays current day on header
 var today = dayjs();
 $('#currentDay').text(today.format('MMM D, YYYY'));
@@ -34,10 +10,32 @@ for (let index = 9; index < 18; index++) {
     });
 }
 
-//Display local storage data for each hour on that hour
+//Loads local storage data to each hour of the day
 window.onload = function() {
     for (let index = 9; index < 18; index++) {
         var storedInput = localStorage.getItem("userInput" + index);
         $("#hour-" + index).find('textarea').first().val(storedInput);
+    }
+}
+
+//Current hour of the day
+var currentTime = dayjs().hour();
+
+//Changes class based on time of day
+for (let index = 9; index < 18; index++) {
+    if (currentTime > index) {
+        $("#hour-" + index).removeClass('future');
+        $("#hour-" + index).removeClass('present');
+        $("#hour-" + index).addClass('past');
+    }
+    else if (currentTime === index) {
+        $("#hour-" + index).removeClass('future');
+        $("#hour-" + index).removeClass('past');
+        $("#hour-" + index).addClass('present');
+    }
+    else {
+        $("#hour-" + index).removeClass('past');
+        $("#hour-" + index).removeClass('present');
+        $("#hour-" + index).addClass('future');
     }
 }
