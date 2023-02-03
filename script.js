40ff19cb8319af7e1b3b8bba9bcb00dca31ca803
Matt Osborne <MattJOzzy@gmail.com>
2023-02-02 19:16:52 -0600

    completed javascript functions to save user input and display that input for each hour

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -18,7 +19,25 @@ $(function () {
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+
   });
-  
+
+// Displays current day on header
+var today = dayjs();
+$('#currentDay').text(today.format('MMM D, YYYY'));
+
+//Getting the save button to save user input to local storage for each hour
+for (let index = 9; index < 18; index++) {
+    var saveButton = $("#hour-" + index).find('button').first().click(function(){
+        var userInput = $("#hour-" + index).find('textarea').first().val();
+        localStorage.setItem("userInput" + index, userInput);
+    });
+}
+
+//Display local storage data for each hour on that hour
+window.onload = function() {
+    for (let index = 9; index < 18; index++) {
+        var storedInput = localStorage.getItem("userInput" + index);
+        $("#hour-" + index).find('textarea').first().val(storedInput);
+    }
+}
